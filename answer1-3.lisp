e05ded764b2bf99d3b387d7e59d60b2639a44c24
Jack Hsu <90829467+xg6636@users.noreply.github.com>
2021-09-16 16:35:39 +0800

    Update answer1-3.lisp
    add no.2 answer

--- answer1-3.lisp
+++ answer1-3.lisp
@@ -7,3 +7,15 @@
   (cond ((null lst) 0)
         ((> 3 (length lst)) 0)
         (t (+ (_betw lst) (count_between (cdr lst))))))
+
+(defun between (lst) 
+  (defun _betw2 (lst2) 
+    (if 
+      (or (and (< (car lst2) (cadr lst2)) (> (caddr lst2) (cadr lst2))) 
+          (and (> (car lst2) (cadr lst2)) (< (caddr lst2) (cadr lst2))))
+      (list (cadr lst2))
+      '()))
+  (cond 
+    ((null lst) '())
+    ((> 3 (length lst)) '())
+    (t (append (_betw2 lst) (between (cdr lst))))))
