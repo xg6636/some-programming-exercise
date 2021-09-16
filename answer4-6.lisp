f45f772f7ddf96c096764b6730369a80e4036911
Jack Hsu <90829467+xg6636@users.noreply.github.com>
2021-09-16 17:26:31 +0800

    Update answer4-6.lisp
    add another answer

--- answer4-6.lisp
+++ answer4-6.lisp
@@ -7,3 +7,16 @@
          (_nf (cdr lst) (append nlst (list (car lst))) nnlst)
          (_nf (cdr lst) nlst (append nnlst (list (car lst))))))))
   (_nf lst '() '()))
+
+(defun transpose_t (lst) 
+  (defun _car (lst) 
+    (cond 
+      ((null lst) '())
+      (t (cons (car (car lst)) (_car (cdr lst))))))
+  (defun _cdr (lst) 
+    (cond 
+      ((null lst) '())
+      (t (cons (cdr (car lst)) (_cdr (cdr lst))))))
+  (cond 
+    ((null (car lst)) '())
+    (t (cons (_car lst) (transpose_t (_cdr lst))))))
